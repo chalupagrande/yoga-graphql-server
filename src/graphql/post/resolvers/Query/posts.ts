924e18dd4e5d751d21b5fd99008aa353d65f545d
Jamie Skinner <jamie@etcetera.cr>
2025-02-19 23:45:56 -0600

    delete generated files

--- src/graphql/post/resolvers/Query/posts.ts
+++
@@ -1,3 +0,0 @@
-
-        import type   { QueryResolvers } from './../../../__generated__/types.generated';
-        export const posts: NonNullable<QueryResolvers['posts']> = async (_parent, _arg, _ctx) => { /* Implement Query.posts resolver logic here */ };
